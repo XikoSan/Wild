c6c993538e754a4d977f901ce5cc6f3a2c1937f0
Andrew Gorshenin <andrey.gorshenin.95@gmail.com>
2021-04-09 16:53:59 +0300

    пароль локальной бд в переменной окружения

--- wild_politics/settings.py
+++ wild_politics/settings.py
@@ -29,7 +29,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.0.197', '188.233.16.66', '192.168.1.237']
 
-
 # Celery Configuration Options
 CELERY_TIMEZONE = "Europe/Moscow"
 CELERY_TASK_TRACK_STARTED = True
@@ -37,7 +36,6 @@ CELERY_TASK_TIME_LIMIT = 60 * 60
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
-
 
 # Application definition
 
@@ -123,7 +121,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'wild_politics',
         'USER': 'postgres',
-        # 'PASSWORD': 'aa10802602',
+        'PASSWORD': os.getenv('DB_PASS'),
         'HOST': 'localhost',
         'PORT': '5432',
         # 'ATOMIC_REQUESTS': 'True',
