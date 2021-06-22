05a953ed0327d9222b9756447b073614d1f2380a
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2021-06-22 22:30:45 +0300

    проверка сентри

--- wild_politics/urls.py
+++ wild_politics/urls.py
@@ -20,8 +20,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
                   path('admin/', admin.site.urls),
+                  path('sentry-debug/', trigger_error),
                   path('', include('social_django.urls', namespace='social')),
                   url(r'', include('player.urls')),
                   url(r'', include('region.urls')),
