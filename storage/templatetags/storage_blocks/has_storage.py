c3566d9705acf88b24effa9c323a7a667a342328
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2021-07-26 20:55:39 +0300

    отображать в складе только не удалённые блокировки

--- storage/templatetags/storage_blocks/has_storage.py
+++ storage/templatetags/storage_blocks/has_storage.py
@@ -6,7 +6,7 @@ from storage.models.good_lock import GoodLock
 @register.inclusion_tag('storage/storage_blocks/has_storage.html')
 def has_storage(storage):
     locks = {}
-    for lock in GoodLock.objects.filter(lock_storage=storage):
+    for lock in GoodLock.objects.filter(lock_storage=storage, deleted=False):
         if lock.lock_good in locks.keys():
             locks[lock.lock_good] += lock.lock_count
         else:
