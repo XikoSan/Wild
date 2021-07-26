7847e09369ebae7b7cd340bb65040cefd3c75cc5
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2021-07-26 20:56:17 +0300

    разработка без сентрика

--- wild_politics/settings.py
+++ wild_politics/settings.py
@@ -80,20 +80,21 @@ sentry_environment = "production"
 if not os.getenv('TOOL'):
     sentry_environment = "development"
 
-sentry_sdk.init(
-    environment=sentry_environment,
-    dsn="https://91bebe2f57014429968500d11b987c5b@o877046.ingest.sentry.io/5827278",
-    integrations=[DjangoIntegration()],
+if not sentry_environment == "development":
+    sentry_sdk.init(
+        environment=sentry_environment,
+        dsn="https://91bebe2f57014429968500d11b987c5b@o877046.ingest.sentry.io/5827278",
+        integrations=[DjangoIntegration()],
 
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
 
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
 
 USE_L10N = True
 USE_THOUSAND_SEPARATOR = True
