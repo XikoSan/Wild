ceed04393e678829874fd2467606a89e561714d3
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2021-09-25 15:02:00 +0300

    Таймер перезарядки энергии

--- player/static/header.js
+++ player/static/header.js
@@ -36,11 +36,22 @@ window.onload = function countdown() {
         var sec_string = $('#refill-countdown').attr('data-text');
         var sec = parseInt(sec_string);
 
+        if (sec == 0) {
+            elem.textContent = refill_button_txt;
+        } else {
+
+            var h = sec/3600 ^ 0 ;
+            var m = (sec-h*3600)/60 ^ 0 ;
+            var s = sec-h*3600-m*60 ;
+            elem.textContent = (m<10?"0"+m:m)+":"+(s<10?"0"+s:s);
+            sec = --sec;
+        }
+
         //запускаем функцию с повторением раз 1 секунду
         var id = setInterval(frame, 1000);
         function frame() {
             if (sec == 0) {
-                elem.textContent = '';
+                elem.textContent = refill_button_txt;
                 clearInterval(id);
             } else {
 
--- player/templatetags/player_status.py
+++ player/templatetags/player_status.py
@@ -2,6 +2,8 @@ from django import template
 
 from storage.models.cash_lock import CashLock
 from django.db.models import Sum
+import time
+from player.views.timers import until_recharge
 
 # from gamecore.all_views.header.until_recharge import UntilRecharge
 register = template.Library()
@@ -9,11 +11,17 @@ register = template.Library()
 
 @register.inclusion_tag('player/player_status.html')
 def player_status(player):
-
     locked = CashLock.objects.filter(lock_player=player, deleted=False).aggregate(total_cash=Sum('lock_cash'))
+
+    seconds = until_recharge(player)
+    time_text = None
+
+    if seconds > 0:
+        time_text = time.strftime('%M:%S', time.gmtime(seconds))
 
     return {
         'player': player,
         'locked': locked['total_cash'],
-        # 'countdown': UntilRecharge(player)
+        'countdown': seconds,
+        'time_text': time_text
     }
