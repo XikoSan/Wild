0068c497817c82c622d0fa4151ff5e388ac3428e
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2021-10-07 22:11:54 +0300

    Просмотр региона

--- region/urls.py
+++ region/urls.py
@@ -1,9 +1,9 @@
 # coding=utf-8
-from django.conf.urls import include, url
-from django.urls import path
+from django.conf.urls import url
 
 from .views.do_mining import do_mining
 from .views.map.map import map
+from .views.map.open_region import open_region
 from .views.map.region_info import region_info
 from .views.mining import mining
 
@@ -18,5 +18,8 @@ urlpatterns = [
     url(r'^map$', map, name='map'),
     # информация о регионе:
     url(r'^info/region/(?P<id>[A-Z]+-[A-Z]+)$', region_info, name='region_info'),
+
+    # Opening page of the state
+    url(r'^region/(?P<pk>\d+)/$', open_region, name='open_region'),
 
 ]
---
+++ region/views/map/open_region.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, render
+
+from party.party import Party
+from player.decorators.player import check_player
+from player.player import Player
+from region.region import Region
+
+
+@login_required(login_url='/')
+@check_player
+# Opening page with selected region data
+def open_region(request, pk):
+    # Получаем объект персонажа, по его ключу
+    # Текущий пользователь
+    player = Player.objects.get(account=request.user)
+
+    region = get_object_or_404(Region, pk=pk)
+
+    players_count = Player.objects.filter(banned=False, region=region).count()
+
+    parties_count = Party.objects.filter(region=region, deleted=False).count()
+
+    # # список войн за все время
+    # war_types = get_subclasses(War)
+    # wars_cnt = 0
+    # # -===============================
+    # # находим для вывода в Овер ближайшую к завершению войну государства
+    # # для каждого типа войн:
+    # for type in war_types:
+    #     # если есть войны такого типа в регионе
+    #     wars_cnt += type.objects.filter(Q(agr_region=region) | Q(def_region=region)).count()
+
+    return render(request, 'region/region_view.html', {
+        'page_name': region.region_name,
+        'player': player,
+        'region': region,
+
+        'players_count': players_count,
+        'parties_count': parties_count,
+
+        # 'wars_cnt': wars_cnt,
+    })
