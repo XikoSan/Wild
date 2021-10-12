b4fdd9c9b3209f0beb83bedabfe8202ecf7286e2
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2021-10-13 00:16:45 +0300

    не создавать задачу на полет

--- region/views/map/map.py
+++ region/views/map/map.py
@@ -31,23 +31,25 @@ def map(request):
         except Region.DoesNotExist:
             raise Exception("Region is doesn't exist")
 
-        player.destination = destination
-        player.save()
-        duration = time_in_flight(player, player.destination)
-        # move_to_another_region.apply_async((player.id,), countdown=duration)
+        if not player.destination:
 
-        start_time = timezone.now() + datetime.timedelta(seconds=duration)
-        clock, created = ClockedSchedule.objects.get_or_create(clocked_time=start_time)
+            player.destination = destination
+            player.save()
+            duration = time_in_flight(player, player.destination)
+            # move_to_another_region.apply_async((player.id,), countdown=duration)
 
-        player.task = PeriodicTask.objects.create(
-            name=str(player.pk) + ' fly ' + str(player.destination.pk),
-            task='move_to_another_region',
-            clocked=clock,
-            one_off=True,
-            args=json.dumps([player.pk]),
-            start_time=timezone.now()
-        )
-        player.save()
+            start_time = timezone.now() + datetime.timedelta(seconds=duration)
+            clock, created = ClockedSchedule.objects.get_or_create(clocked_time=start_time)
+
+            player.task = PeriodicTask.objects.create(
+                name=str(player.pk) + ' fly ' + str(player.destination.pk),
+                task='move_to_another_region',
+                clocked=clock,
+                one_off=True,
+                args=json.dumps([player.pk]),
+                start_time=timezone.now()
+            )
+            player.save()
 
     response = render(request, 'region/map.html', {
         'page_name': _('Карта'),
