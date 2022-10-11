f98bd21d2ba90e5d9962ea70394568530a26502f
Андрей Горшенин <m4troskun>
2022-10-11 16:10:07 +0300

    рейтинг в профиле других игроков

--- player/views/view_profile.py
+++ player/views/view_profile.py
@@ -3,6 +3,7 @@ import redis
 from allauth.socialaccount.models import SocialAccount
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
+from django.db import connection
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 
@@ -50,11 +51,11 @@ def view_profile(request, pk):
     # if PlayerSettings.objects.filter(player=char).exists():
     #     char_settings = PlayerSettings.objects.get(player=char)
     # ---------------------
-    # cursor = connection.cursor()
-    # cursor.execute(
-    #     "SELECT COUNT(DISTINCT store.cash + player.cash) FROM gamecore_player AS player JOIN gamecore_storage AS store ON store.owner_id = player.id WHERE store.cash + player.cash >= (SELECT store.cash + player.cash FROM gamecore_player AS player JOIN gamecore_storage AS store ON store.owner_id = player.id WHERE player.id=%s LIMIT 1);",
-    #     [char.pk])
-    # cash_rating = cursor.fetchone()
+    cursor = connection.cursor()
+    cursor.execute(
+        "with sum_limit (lim) as ( select SUM(store.cash) + player.cash from player_player as player join storage_storage as store on player.id = %s and store.owner_id = player.id and store.deleted = false group by player.id ), store_sum (owner_id, cash) as ( select store.owner_id, sum(store.cash) from storage_storage as store where store.deleted = false group by store.owner_id ) select count ( * ) + 1 from player_player as player join store_sum as store on store.owner_id = player.id where store.cash + player.cash > ( select lim from sum_limit );",
+        [char.pk])
+    cash_rating = cursor.fetchone()
     # ---------------------
 
     party_back = True
@@ -72,6 +73,7 @@ def view_profile(request, pk):
                                   'minister': minister,
                                   'dtime': dtime,
                                   'user_link': user_link,
+                                  'cash_rating': cash_rating[0],
 
                                   'party_back': party_back,
 
