553af6113e78def569951372a34defedf8732170
Андрей Горшенин <m4troskun>
2022-10-23 20:55:45 +0300

    листание страниц универсального списка

---
+++ player/templatetags/redesign/pagination.py
@@ -0,0 +1,11 @@
+from django import template
+
+# from gamecore.all_views.header.until_recharge import UntilRecharge
+register = template.Library()
+
+
+@register.inclusion_tag('player/redesign/lists/pagination.html')
+def pagination(page):
+    return {
+        'page': page,
+    }
