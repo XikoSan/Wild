e76e7cd9b9d27df57fc4e4fee4e0631d896bd4e3
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2022-12-30 21:32:52 +0300

    чат работающий и локально

--- player/views/overview.py
+++ player/views/overview.py
@@ -1,7 +1,9 @@
 import datetime
 import json
-import pytz
+import os
 import random
+
+import pytz
 import redis
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
@@ -10,15 +12,14 @@ from django.utils import timezone
 from django.utils.translation import ugettext as _
 
 from chat.models.sticker import Sticker
-from chat.models.sticker_pack import StickerPack
 from chat.models.stickers_ownership import StickersOwnership
 from gov.models.president import President
 from gov.models.presidential_voting import PresidentialVoting
 from party.party import Party
 from player.decorators.player import check_player
+from player.game_event.game_event import GameEvent
 from player.player import Player
 from player.views.get_subclasses import get_subclasses
-from polls.models.poll import Poll
 from region.region import Region
 from region.views.lists.get_regions_online import get_region_online
 from state.models.parliament.parliament import Parliament
@@ -26,9 +27,7 @@ from state.models.parliament.parliament_party import ParliamentParty
 from state.models.parliament.parliament_voting import ParliamentVoting
 from state.models.state import State
 from war.models.wars.war import War
-from wild_politics.settings import TIME_ZONE, sentry_environment
-
-from player.game_event.game_event import GameEvent
+from wild_politics.settings import TIME_ZONE
 
 
 # главная страница
@@ -135,7 +134,7 @@ def overview(request):
             stickers_dict[sticker_own.pack.pk] = Sticker.objects.filter(pack=sticker_own.pack)
 
     http_use = False
-    if sentry_environment == "development":
+    if os.getenv('HTTP_USE'):
         http_use = True
 
     # polls = Poll.actual.all()
