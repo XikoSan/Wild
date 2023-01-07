e29ef976e6e2015721d8373bcadc656de2801feb
Андрей Горшенин <m4troskun>
2023-01-07 21:49:27 +0300

    последний онлайн в списке партий

--- party/views/open_party.py
+++ party/views/open_party.py
@@ -1,3 +1,6 @@
+import pytz
+import redis
+from datetime import datetime
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render
 
@@ -5,6 +8,7 @@ from party.logs.party_apply import PartyApply
 from party.party import Party
 from player.decorators.player import check_player
 from player.player import Player
+from wild_politics.settings import TIME_ZONE
 
 
 @login_required(login_url='/')
@@ -19,6 +23,18 @@ def open_party(request, pk):
 
     party_characters = Player.objects.only('pk', 'party').filter(party=party)
 
+    online_dict = {}
+
+    if party_characters:
+        r = redis.StrictRedis(host='redis', port=6379, db=0)
+
+        for char in party_characters:
+            timestamp = r.hget('online', str(char.pk))
+            if timestamp:
+                online_dict[char.pk] = datetime.fromtimestamp(int(timestamp)).replace(
+                    tzinfo=pytz.timezone(TIME_ZONE)).astimezone(
+                    tz=pytz.timezone(player.time_zone)).strftime("%d.%m.%Y")
+
     return render(request, 'party/party_view.html', {
         'page_name': party.title,
         'player': player,
@@ -27,6 +43,7 @@ def open_party(request, pk):
         'has_request': PartyApply.objects.filter(player=player, party=party, status='op').exists(),
 
         'party_characters': party_characters,
+        'online_dict': online_dict,
         'players_count': party_characters.count(),
 
     })
