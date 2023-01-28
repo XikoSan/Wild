f40895de715f414567cb55c008485deff2139b55
Андрей Горшенин <m4troskun>
2023-01-28 23:26:36 +0300

    обновление жестом

--- player/static/redesign/js/header.js
+++ player/static/redesign/js/header.js
@@ -99,3 +99,56 @@ modalCancel.addEventListener('click', () => modal.classList.remove('active'))
       this.domNodes.main.classList.remove('active');
     }
   }
+
+// mobile refresh page
+  if (window.innerWidth < 600) {
+
+    document.body.insertAdjacentHTML('afterbegin', `
+    <svg class="svg-reload" fill="var(--c10two)" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path d="m11.998 2.001c5.517 0 9.997 4.48 9.997 9.997 0 5.518-4.48 9.998-9.997 9.998-5.518 0-9.998-4.48-9.998-9.998 0-5.517 4.48-9.997 9.998-9.997zm-4.496 6.028-.002-.825c0-.414-.336-.75-.75-.75s-.75.336-.75.75v3.048c0 .414.336.75.75.75h3.022c.414 0 .75-.336.75-.75s-.336-.756-.75-.756h-1.512c.808-1.205 2.182-1.998 3.74-1.998 2.483 0 4.5 2.016 4.5 4.5 0 2.483-2.017 4.5-4.5 4.5-1.956 0-3.623-1.251-4.242-2.997-.106-.299-.389-.499-.707-.499-.518 0-.88.513-.707 1.001.825 2.327 3.048 3.995 5.656 3.995 3.312 0 6-2.689 6-6 0-3.312-2.688-6-6-6-1.79 0-3.399.786-4.498 2.031z" fill-rule="nonzero"/>
+    </svg>
+    `)
+
+    const refreshSvg = document.querySelector('.svg-reload');
+
+    const TOUCHMOVE_PATH_LENGTH = 150;
+    const MAX_Y_FROM_SCREEN_TOP= 75;
+    const MAX_SVG_MOVE_PATH = 400;
+    const SVG_SIZE = 50;
+
+    document.addEventListener('touchstart', (evt) => {
+
+      console.log(evt.touches[0].clientY);
+      if (evt.touches[0].clientY > MAX_Y_FROM_SCREEN_TOP) return;
+
+      let startY = evt.touches[0].clientY;
+      let isRefreshAvailable = false;
+
+      function touchMoveHandler(evt) {
+        const currentY = evt.touches[0].clientY;
+        let yDiff = currentY - startY;
+        if (yDiff > TOUCHMOVE_PATH_LENGTH) {
+          isRefreshAvailable = true;
+          if (yDiff > MAX_SVG_MOVE_PATH) yDiff = MAX_SVG_MOVE_PATH;
+          refreshSvg.style.transform = `translateY(${yDiff - TOUCHMOVE_PATH_LENGTH/2 - SVG_SIZE}rem) rotate(${yDiff - TOUCHMOVE_PATH_LENGTH}deg) scaleX(-1)`;
+          refreshSvg.style.filter = 'grayscale(0)';
+        } else {
+          isRefreshAvailable = false;
+          refreshSvg.style.transform = `translateY(${yDiff - TOUCHMOVE_PATH_LENGTH/2 - SVG_SIZE}rem) rotate(${yDiff - TOUCHMOVE_PATH_LENGTH}deg) scaleX(-1)`;
+          refreshSvg.style.filter = 'grayscale(1)';
+        }
+      }
+
+      function touchEndHandler(evt) {
+        if (isRefreshAvailable) document.location.reload();
+        else { refreshSvg.style.transform = `translateY(${-SVG_SIZE}rem) rotate(0deg) scaleX(-1)`;}
+        document.removeEventListener('touchmove', touchMoveHandler)
+        document.removeEventListener('touchend', touchEndHandler)
+      }
+
+      document.addEventListener('touchmove', touchMoveHandler);
+      document.addEventListener('touchend', touchEndHandler);
+
+    })
+
+  }
