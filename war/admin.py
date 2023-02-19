92085bc997c51b8170fa6ff55963a8f28e8e8957
Андрей Горшенин <m4troskun>
2023-02-19 15:37:23 +0300

    косметические правки админки для отрядов

--- war/admin.py
+++ war/admin.py
@@ -8,11 +8,34 @@ from war.models.wars.event_war import EventWar
 from war.models.wars.ground_war import GroundWar
 from war.models.wars.war_side import WarSide
 
+
+class SquadAdmin(admin.ModelAdmin):
+    search_fields = ['owner__nickname',]
+    raw_id_fields = ('owner',)
+    list_display = ['owner', 'content_type', 'object_id', 'get_agr_region', 'get_def_region', ]
+
+    def get_agr_region(self, obj):
+        war_class = obj.content_type.model_class()
+
+        if war_class.objects.filter(pk=obj.object_id).exists():
+            return str(workplace_class.objects.get(pk=obj.object_id).agr_region.region_name)
+        else:
+            return ''
+
+    def get_def_region(self, obj):
+        war_class = obj.content_type.model_class()
+
+        if war_class.objects.filter(pk=obj.object_id).exists():
+            return str(workplace_class.objects.get(pk=obj.object_id).def_region.region_name)
+        else:
+            return ''
+
 # Register your models here.
 admin.site.register(EventWar)
 admin.site.register(GroundWar)
-admin.site.register(Recon)
-admin.site.register(Infantry)
-admin.site.register(LightVehicle)
-admin.site.register(HeavyVehicle)
 admin.site.register(WarSide)
+
+admin.site.register(Recon, SquadAdmin)
+admin.site.register(Infantry, SquadAdmin)
+admin.site.register(LightVehicle, SquadAdmin)
+admin.site.register(HeavyVehicle, SquadAdmin)
