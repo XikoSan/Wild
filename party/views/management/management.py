ef1b2d2bb83dec716b4f9c0e6c70f732e7ddc14f
Андрей Горшенин <m4troskun>
2023-02-23 00:18:27 +0300

    редизайн страницы без партии

--- party/views/management/management.py
+++ party/views/management/management.py
@@ -13,23 +13,27 @@ from player.player import Player
 def management(request):
     # получаем персонажа
     player = Player.get_instance(account=request.user)
-    # если игрок действительно лидер партии
-    if player.party_post.party_lead:
-        # если у лидера партии есть партия
-        if Party.objects.filter(pk=player.party.pk).exists():
-            # получаем партию
-            party = Party.objects.get(pk=player.party.pk)
+    if player.party:
+        # если игрок действительно лидер партии
+        if player.party_post.party_lead:
+            # если у лидера партии есть партия
+            if Party.objects.filter(pk=player.party.pk).exists():
+                # получаем партию
+                party = Party.objects.get(pk=player.party.pk)
 
-            return render(request, 'party/manage.html',
-                          {'player': player,
-                           'party': party,
-                           'members_count': Player.objects.filter(party=player.party).count(),
-                           'roles_list': PartyPosition.objects.filter(party=player.party),
-                           'roles_count': PartyPosition.objects.filter(party=player.party).count()
-                           })
+                return render(request, 'party/manage.html',
+                              {'player': player,
+                               'party': party,
+                               'members_count': Player.objects.filter(party=player.party).count(),
+                               'roles_list': PartyPosition.objects.filter(party=player.party),
+                               'roles_count': PartyPosition.objects.filter(party=player.party).count()
+                               })
+            else:
+                # во избежание глюков отправляем в партию
+                return redirect('party')
         else:
-            # во избежание глюков отправляем в партию
+            # игроки без лидерки идут домой
             return redirect('party')
     else:
-        # игроки без лидерки идут домой
+        # игроки без партии идут домой
         return redirect('party')
