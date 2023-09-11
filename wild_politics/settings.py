c3687e242d6a53c17fef7db540f901c4ec577cb1
Андрей Горшенин <m4troskun>
2023-09-11 15:45:54 +0300

    новый адрес
    # Conflicts:
    #	wild_politics/settings.py

--- wild_politics/settings.py
+++ wild_politics/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = not os.getenv('PROD')
 
-ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1', '192.168.0.150', 'test.wildpolitics.online', 'wildpolitics.ru']
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1', '192.168.0.150',
+                 'test.wildpolitics.online', 'test.wildpolitics.ru', 'wildpolitics.ru']
 
 INTERNAL_IPS = ['127.0.0.1']
 
