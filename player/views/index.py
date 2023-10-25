e2064350f327632ad525a2176e6e7ceca6871726
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2023-10-26 01:07:29 +0300

    дата открытия сервера

--- player/views/index.py
+++ player/views/index.py
@@ -2,7 +2,7 @@
 
 from django.shortcuts import redirect
 from django.shortcuts import render
-
+from datetime import datetime
 
 def index(request):
 
@@ -10,11 +10,25 @@ def index(request):
         return redirect('overview')
 
     else:
+
+        from player.logs.print_log import log
+        log(datetime.now())
+
+        open_date_str = "2023-10-30 18:00:00"
+        open_date = datetime.strptime(open_date_str, "%Y-%m-%d %H:%M:%S")
+
+        log(open_date)
+
+        is_open = False
+        if datetime.now() > open_date:
+            is_open = True
+
         lang = None
         language = request.LANGUAGE_CODE
         if language != 'ru':
             lang = 'en'
 
         return render(request, 'player/index.html', {
-            'lang': lang
+            'lang': lang,
+            'is_open': is_open
         })
