6dd230f04bf7d71e97dafce5b279b5e9094255e4
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2023-10-29 23:09:28 +0300

    баг загрузки картинки

--- player/views/my_profile.py
+++ player/views/my_profile.py
@@ -49,7 +49,7 @@ def my_profile(request):
             image = Image.open(player.image)
             cropped_image = image.crop((x, y, w + x, h + y))
             resized_image = cropped_image.resize((250, 250), Image.ANTIALIAS)
-            resized_image.save(player.image.path)
+            resized_image.save(player.image.path, 'PNG')
 
             return redirect('my_profile')
     else:
