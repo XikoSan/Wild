8e62b09c934db4efbb1a31e6e397ceb99dff1571
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2023-12-04 12:47:13 +0300

    фикс подачи ЗП

--- bill/static/new_bill.js
+++ bill/static/new_bill.js
@@ -7,15 +7,22 @@ jQuery(document).ready(function ($) {
         sending_data.append('bill_type', current_bill);
         sending_data.append('construction_regions', document.getElementById('construction_default_region').dataset.value);
         sending_data.append('explore_regions', document.getElementById('explore_resources_default_region').dataset.value);
-        sending_data.append('war_region_from', document.getElementById('start_war_default_region').dataset.value);
-        sending_data.append('war_region_to', document.getElementById('start_war_default_victim').dataset.value);
+        sending_data.append('change_taxes_regions', document.getElementById('change_taxes_default_region').dataset.value);
 
         var ind_default = document.getElementById('independence_default_region');
-
         if (ind_default) {
             sending_data.append('independence_regions', ind_default.dataset.value);
         }
-        sending_data.append('change_taxes_regions', document.getElementById('change_taxes_default_region').dataset.value);
+
+        var agr_default = document.getElementById('start_war_default_region');
+        if (agr_default) {
+            sending_data.append('war_region_from', agr_default.dataset.value);
+        }
+
+        var def_default = document.getElementById('start_war_default_victim');
+        if (def_default) {
+            sending_data.append('war_region_to', def_default.dataset.value);
+        }
 
         $.ajax({
               url: "/new_bill/",
