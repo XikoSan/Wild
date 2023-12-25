910d3b4fd43afe013d7179cccbf019134b7e93b7
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2023-12-25 17:56:32 +0300

    правки лутбоксов

--- player/views/generate_rewards.py
+++ player/views/generate_rewards.py
@@ -4,7 +4,7 @@ import random
 # генератор открытия
 def generate_rewards():
     # выбираем количество получаемых игроком наград
-    count = random.choices([1, 2, 3,], weights=[70, 20, 10,])
+    count = random.choices([1, 2, 3,], weights=[85, 10, 5,])
 
     # выбираем какие награды именно игрок получит
     rewards = []
@@ -28,7 +28,7 @@ def generate_rewards():
             for element in diff:
 
                 if element == 'gold':
-                    weights.append(10)
+                    weights.append(15)
 
                 if element == 'premium':
                     weights.append(5)
@@ -53,19 +53,19 @@ def generate_rewards():
 
         if nagrada == 'gold':
 
-            weights = [55, 35, 9, 1,]
+            weights = [84, 10, 5, 1,]
 
             reward_sum = random.choices([500, 1000, 3000, 100000, ], weights=weights)
 
         if nagrada == 'premium':
 
-            weights = [70, 20, 9, 1,]
+            weights = [80, 10, 9, 1,]
 
             reward_sum = random.choices([14, 30, 90, 365, ], weights=weights)
 
         if nagrada == 'wild_pass':
 
-            weights = [70, 20, 9, 1,]
+            weights = [91, 5, 3, 1,]
 
             reward_sum = random.choices([1, 3, 5, 10, ], weights=weights)
 
