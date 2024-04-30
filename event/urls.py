8064a5d001eda3b02164978006d67e4fe0e02798
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2024-04-30 18:47:53 +0300

    просмотр приглашенных игроком

--- event/urls.py
+++ event/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls import url
 from event.views.inviting_event import inviting_event
 from event.views.activate_invite import activate_invite
+from event.views.invited_list import invited_list
 
 urlpatterns = [
     # открыть страницу ивента
@@ -9,4 +10,7 @@ urlpatterns = [
 
     # активация бонус-кода
     url(r'^activate_invite/', activate_invite, name='activate_invite'),
+
+    # Приглашенные игроком
+    url(r'^invited/(?P<pk>\d+)/$', invited_list, name='invited_list'),
 ]
---
+++ event/views/invited_list.py
@@ -0,0 +1,50 @@
+import datetime
+import json
+import pytz
+import random
+import redis
+from datetime import timedelta
+from django.apps import apps
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.utils.translation import pgettext
+from django.utils.translation import ugettext as _
+
+from event.models.inviting_event.invite import Invite
+from player.decorators.player import check_player
+from django.db import connection
+from event.models.inviting_event.cash_event import CashEvent
+from player.logs.cash_log import CashLog
+
+
+# главная страница
+@login_required(login_url='/')
+@check_player
+def invited_list(request, pk):
+
+    if not CashEvent.objects.filter(running=True, event_start__lt=timezone.now(),
+                                    event_end__gt=timezone.now()).exists():
+        return redirect('overview')
+
+    Player = apps.get_model('player.Player')
+
+    player = Player.get_instance(account=request.user)
+
+    if not Player.objects.filter(pk=pk).exists():
+        return redirect('overview')
+
+    char = Player.get_instance(pk=pk)
+
+    invited_list = Invite.objects.filter(sender=char)
+
+    page = 'event/invited_list.html'
+    # отправляем в форму
+    response = render(request, page, {
+        'page_name': f'Приглашённые игроком {char.nickname}',
+
+        'player': player,
+        'invited_list': invited_list,
+    })
+
+    return response
