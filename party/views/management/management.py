8b6661e742ddab991b1239a1b676935eb5feaee6
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2024-05-21 00:29:33 +0300

    выдача золота только тем, кто в партии более недели

--- party/views/management/management.py
+++ party/views/management/management.py
@@ -1,12 +1,16 @@
+from datetime import timedelta
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.utils.translation import pgettext
 from itertools import chain
 
+from party.logs.membership_log import MembershipLog
 from party.party import Party
 from party.position import PartyPosition
 from player.decorators.player import check_player
 from player.player import Player
-from django.utils.translation import pgettext
+
 
 @login_required(login_url='/')
 @check_player
@@ -19,9 +23,17 @@ def management(request):
 
                 groups = list(player.account.groups.all().values_list('name', flat=True))
                 page = 'party/manage.html'
-                
+
                 if 'redesign' not in groups:
                     page = 'party/redesign/manage.html'
+
+                # получаем список логов тех, кто вступил ранее, чем за неделю до текущего момента
+                member_logs = MembershipLog.objects.filter(dtime__lt=timezone.now() - timedelta(days=7),
+                                                           party=party,
+                                                           exit_dtime=None)
+                member_pks = []
+                for log in member_logs:
+                    member_pks.append(log.player.pk)
 
                 return render(request, page,
                               {
@@ -32,8 +44,8 @@ def management(request):
                                   'roles_count': PartyPosition.objects.filter(party=player.party).count(),
 
                                   'members_count': Player.objects.filter(party=player.party).count(),
-                                  'members': Player.objects.filter(party=player.party)
-                               })
+                                  'members': Player.objects.filter(pk__in=member_pks, party=player.party)
+                              })
             else:
                 return redirect('party')
         else:
