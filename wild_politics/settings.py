5dc228c330cd7b2478c59c1b01b920ea3efa54bd
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2024-07-06 13:00:48 +0300

    Revert "отладка в проде"
    This reverts commit 043c2e00a3ff50a4bac54b91ef11e28f46a2a741.

--- wild_politics/settings.py
+++ wild_politics/settings.py
@@ -28,8 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = not os.getenv('PROD')
-DEBUG = True
+DEBUG = not os.getenv('PROD')
 
 ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1', '192.168.0.150', '192.168.0.181', '307f41365796.vps.myjino.ru',
                  'test.wildpolitics.online', 'test.wildpolitics.ru', 'wildpolitics.ru']
@@ -295,7 +294,8 @@ SESSION_COOKIE_AGE = 259200
 DATA_UPLOAD_MAX_MEMORY_SIZE = 20971520
 
 # отладочная печать в Docker
-level = 'INFO'
+# level = 'INFO'
+level = 'DEBUG'
 
 if DEBUG:
     level = 'DEBUG'
