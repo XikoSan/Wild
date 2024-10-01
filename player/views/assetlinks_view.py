c29616ab26e6e5c6a470776d4fb64839c05853d6
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2024-10-01 09:56:36 +0300

    исправление assetlinks

--- player/views/assetlinks_view.py
+++ player/views/assetlinks_view.py
@@ -1,11 +1,9 @@
 import os
-from django.http import JsonResponse
-
+from django.http import HttpResponse
 from wild_politics.settings import BASE_DIR
-
 
 def assetlinks_view(request):
     file_path = os.path.join(BASE_DIR, '.well-known', 'assetlinks.json')
     with open(file_path, 'r') as file:
         data = file.read()
-    return JsonResponse(data, safe=False)
+    return HttpResponse(data, content_type='application/json')
