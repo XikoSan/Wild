360a3f22a52efb914a68b35877cc6b351cb8e65a
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2024-10-28 20:50:35 +0300

    фикс ошибки страницы самолётов

--- player/views/customization/plane/plane_select.py
+++ player/views/customization/plane/plane_select.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
 from django.shortcuts import render
-from django.utils.translation import gettext_lazy
+from django.utils.translation import pgettext
 
 from player.decorators.player import check_player
 from player.player import Player
