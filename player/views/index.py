330af1840f5369b634b033bc32f38c5ddd04b50b
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2024-10-29 18:14:04 +0300

    правка начального языка для иностранцев

--- player/views/index.py
+++ player/views/index.py
@@ -3,6 +3,8 @@
 from django.shortcuts import redirect
 from django.shortcuts import render
 from datetime import datetime
+from django.utils import translation
+from django.conf import settings
 
 def index(request):
 
@@ -18,12 +20,25 @@ def index(request):
         if datetime.now() > open_date:
             is_open = True
 
+        from player.logs.print_log import log
+        log(request.LANGUAGE_CODE)
+
         lang = None
         language = request.LANGUAGE_CODE
         if language != 'ru':
             lang = 'en'
+            translation.activate(lang)
 
-        return render(request, 'player/index.html', {
+        log(lang)
+
+        # Формируем ответ с рендерингом шаблона
+        response = render(request, 'player/index.html', {
             'lang': lang,
             'is_open': is_open
         })
+
+        # Устанавливаем cookie для языка, если он не 'ru'
+        if language != 'ru':
+            response.set_cookie(settings.LANGUAGE_COOKIE_NAME, 'en', max_age=30 * 24 * 60 * 60)  # Cookie на 30 дней
+
+        return response
