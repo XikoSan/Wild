680ce2854e610855b14d95582de3446929c746de
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2024-11-15 01:03:41 +0300

    шапка ивента

--- player/views/game_event/play_market_event.py
+++ player/views/game_event/play_market_event.py
@@ -8,6 +8,8 @@ from django.db.models import Sum
 from django.shortcuts import redirect
 from django.shortcuts import render
 from django.utils import timezone
+from django.utils import translation
+from django.utils.translation import check_for_language
 
 from player.decorators.player import check_player
 from player.player import Player
@@ -22,6 +24,19 @@ def play_market_event(request):
     player = Player.get_instance(account=request.user)
 
     # ---------------------
+    PlayerSettings = apps.get_model('player.PlayerSettings')
+
+    en_lang = False
+
+    language = request.LANGUAGE_CODE
+    if language != 'ru':
+        en_lang = True
+    else:
+        if PlayerSettings.objects.filter(player=player).exists():
+            player_settings = PlayerSettings.objects.get(player=player)
+            if player_settings.language != 'ru':
+                en_lang = True
+        # ---------------------
 
     page = 'player/redesign/game_event/play_market_event.html'
 
@@ -29,6 +44,7 @@ def play_market_event(request):
         'page_name': pgettext('event', 'Наступление на Google Play'),
 
         'player': player,
+        'en_lang': en_lang,
     })
 
     return response
