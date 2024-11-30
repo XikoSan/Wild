b32a9f2cda4b18272ca736b58e6e2a9da5662a6b
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2024-11-30 04:45:31 +0300

    вывод ЗП президента

--- state/views/open_state.py
+++ state/views/open_state.py
@@ -1,15 +1,21 @@
+from datetime import timedelta
 from django.contrib.auth.decorators import login_required
+from django.db.models import Sum, F, FloatField, Case, When
 from django.shortcuts import get_object_or_404, redirect, render
-from region.building.power_plant import PowerPlant
+from django.utils.timezone import now
+from math import ceil
+
+from gov.models.minister import Minister
+from gov.models.president import President
+from gov.models.presidential_voting import PresidentialVoting
+from metrics.models.daily_gold_by_state import DailyGoldByState
 from party.party import Party
 from player.decorators.player import check_player
 from player.player import Player
+from region.building.power_plant import PowerPlant
 from region.models.region import Region
 from state.models.capital import Capital
 from state.models.state import State
-from gov.models.president import President
-from gov.models.presidential_voting import PresidentialVoting
-from gov.models.minister import Minister
 
 
 @login_required(login_url='/')
@@ -37,7 +43,8 @@ def open_state(request, pk):
         president = President.objects.get(state=state)
 
         if PresidentialVoting.objects.filter(president=president, running=False).exists():
-            last_voting = PresidentialVoting.objects.filter(president=president, running=False).order_by('-voting_end').first()
+            last_voting = PresidentialVoting.objects.filter(president=president, running=False).order_by(
+                '-voting_end').first()
 
         if Minister.objects.filter(state=state).exists():
             ministers = Minister.objects.filter(state=state)
@@ -59,13 +66,46 @@ def open_state(request, pk):
     parties = Party.objects.filter(region__in=regions_state, deleted=False)
 
     party_characters_pk = Player.objects.only('pk', 'party').filter(party__in=parties)
-    
+
     party_sizes = {}
     for char in party_characters_pk:
         if char.party in party_sizes:
             party_sizes[char.party] += 1
         else:
             party_sizes[char.party] = 1
+
+    # ----------
+    percentage = 0
+    salary = 0
+
+    if president:
+        # Вычисляем начало недели (понедельник)
+        monday_start = now() - timedelta(days=now().weekday())
+        seven_days_ago = monday_start.replace(hour=0, minute=0, second=0, microsecond=0)
+
+        # 2. Суммируем заработанное всеми государствами за последние 7 дней
+        total_gold_last_7_days = DailyGoldByState.objects.filter(
+            date__gte=seven_days_ago
+        ).aggregate(total_gold=Sum('daily_gold'))['total_gold'] or 0
+
+        # 3. Получаем данные по каждому государству: сколько они заработали и какой это процент
+        percentage = DailyGoldByState.objects.filter(
+            date__gte=seven_days_ago,
+            state__pk=pk
+        ).annotate(
+            state_total=Sum('daily_gold'),
+            percentage=Case(
+                When(state_total=0, then=0.0),  # Если заработок нулевой
+                default=F('state_total') / total_gold_last_7_days,  # Иначе вычисляем процент
+                output_field=FloatField(),
+            )
+        ).values_list('percentage', flat=True).first()
+
+        if not percentage:
+            percentage = 0
+
+        salary = ceil(15000 * percentage * 0.33)
+    # ----------
 
     return render(request, 'state/redesign/state_view.html', {
         'page_name': state.title,
@@ -89,6 +129,8 @@ def open_state(request, pk):
 
         'energy_production': PowerPlant.get_power_production(state=state),
         'energy_consumption': PowerPlant.get_power_consumption(state=state),
+
+        'salary': salary,
 
         # 'wars_cnt': wars_cnt,
     })
