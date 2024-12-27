39bc13005d92c6ba369490220df19143046d8100
Андрей Горшенин <andrey.gorshenin.95@gmail.com>
2024-12-27 22:31:26 +0300

    скобку пропустил

--- storage/static/js/redesign/lootbox_socket.js
+++ storage/static/js/redesign/lootbox_socket.js
@@ -32,7 +32,7 @@ function connectWebSocket() {
                     globWebSocket = connectWebSocket();
                 }, reconnectInterval);
             } else {
-                display_modal('notify', conn_header, conn_body, null, conn_ok;
+                display_modal('notify', conn_header, conn_body, null, conn_ok);
             }
         }
     };
